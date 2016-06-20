3c3b7a39fcb011fdf358758f8042cde3b47f8637
Paul Berkman <pberkman@expedia.com>
2016-06-20 20:28:17 +1000

    updated apps to commence parsing the extractor

---
+++ whatpolliessay/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pberkman/Workspace/Personal/whatpolliessay/whatpolliessay/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
